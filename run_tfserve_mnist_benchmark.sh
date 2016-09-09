275c135a8c115f005867a04c2501fd80df836e2a
Dan Crankshaw <dscrankshaw@gmail.com>
2016-09-08 17:28:25 -0700

    added command to run no rpc benchmarker

---
+++ run_tfserve_mnist_benchmark.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# ./bazel-bin/tensorflow_serving/example/no_rpc_benchmarker \
+#   --mnist_file=/crankshaw-local/mnist/data/test.data \
+#   --num_requests=1000 --num_batch_threads=2 \
+#   /crankshaw-local/tf_models/mnist_from_guilio/mnist_convnet/tf_serving_model/convnet
+./bazel-bin/tensorflow_serving/example/no_rpc_benchmarker \
+  --mnist_file=/crankshaw-local/mnist/data/test.data \
+  --num_requests=1000000 \
+  --num_batch_threads=2 \
+  /crankshaw-local/tf_models/mnist_from_guilio/mnist_convnet/tf_serving_model/convnet
+  # /crankshaw-local/tf_models/tfserve/mnist_conv_b64/
+
